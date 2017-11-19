fc4aa19bf94aa3317d6dd70ce758dd08256862c8
anatollko <anatollko91@gmail.com>
2017-11-20 01:39:25 +0200

    Добавляет базовые функции (сортировка, макс число, округление)

--- js/stat.js
+++ js/stat.js
@@ -1,5 +1,61 @@
 'use strict';
 
+// typical functions
+
+var getMaxItem = function (arr) {
+  var max = 0;
+  var maxIndex = 0;
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      maxIndex = i;
+      max = arr[i];
+    }
+  }
+  return max;
+};
+
+var sortArray = function (arr) {
+  for (var i = 0; i < arr.length - 1; i++) {
+    var max = arr[i];
+    for (var j = i + 1; j < arr.length; j++) {
+      if (arr[j] > max) {
+        max = arr[j];
+        var buffer = arr[i];
+        arr[i] = max;
+        arr[j] = buffer;
+      }
+    }
+  }
+  return arr;
+};
+
+var sortAssociatedArray = function (arr1, arr2) {
+  for (var i = 0; i < arr1.length - 1; i++) {
+    var max = arr1[i];
+    for (var j = i + 1; j < arr1.length; j++) {
+      if (arr1[j] > max) {
+        max = arr1[j];
+        var buffer1 = arr1[i];
+        var buffer2 = arr2[i];
+        arr1[i] = max;
+        arr1[j] = buffer1;
+        arr2[i] = arr2[j];
+        arr2[j] = buffer2;
+      }
+    }
+  }
+  return [arr1, arr2];
+};
+
+var transferToSeconds = function (arr) {
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = (arr[i] / 1000).toFixed(2);
+  }
+  return arr;
+};
+
+
 window.renderStatistics = function (ctx, names, times) {
 
   // cloud canvas
@@ -57,4 +113,6 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Ура вы победили!', 140, 35);
   ctx.fillText('Список результатов:', 140, 60);
 
+
 };
+
