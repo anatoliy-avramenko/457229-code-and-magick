c4292064a5b8756a07909603a5905a376e0e082e
anatollko <anatollko91@gmail.com>
2017-11-25 23:35:56 +0200

    Добавляет setup.js (окно с похожими магами)

---
+++ js/setup.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var similarHeroes = document.querySelector('.setup');
+similarHeroes.classList.remove('hidden');
+
+document.querySelector('.setup-similar').classList.remove('hidden');
