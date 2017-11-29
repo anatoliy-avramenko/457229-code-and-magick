1d229b127d097c1d2c4481cf67a0c60e501c3c7a
anatollko <anatollko91@gmail.com>
2017-11-29 02:04:42 +0200

    добавляет обработчики для открытия закрытия модального окна

--- js/setup.js
+++ js/setup.js
@@ -73,6 +73,62 @@ wizardList.appendChild(fragment);
 
 
 // раcхайдить скрытые элементы
-document.querySelector('.setup').classList.remove('hidden');
+// document.querySelector('.setup').classList.remove('hidden');
 document.querySelector('.setup-similar').classList.remove('hidden');
 
+// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+
+var ENTER_KEYCODE = 13;
+var ESC_KEYCODE = 27;
+var setupOpen = document.querySelector('.setup-open');
+var setup = document.querySelector('.setup');
+var setupClose = document.querySelector('.setup-close');
+var setupUserName = document.querySelector('.setup-user-name');
+var setupSubmit = document.querySelector('.setup-submit');
+
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscDown);
+};
+
+
+// открывает попап, по нажатию Esc = закрывает, кроме случая, когда input в фокусе
+var openPopup = function () {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscDown);
+  setupUserName.addEventListener('blur', function () {
+    document.addEventListener('keydown', onPopupEscDown);
+  });
+  setupUserName.addEventListener('focus', function () {
+    document.removeEventListener('keydown', onPopupEscDown);
+  });
+};
+
+var onPopupEscDown = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+setupOpen.addEventListener('click', openPopup);
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', closePopup);
+
+setupSubmit.addEventListener('click', function (evt) {
+  closePopup();
+  evt.preventDefault();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
