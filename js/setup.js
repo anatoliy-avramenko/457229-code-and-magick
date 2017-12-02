ca246a1384caa153f74d4dc4fe0a42975111e760
Anatoliy <anatollko91@gmail.com>
2017-12-02 02:35:19 +0200

    добавляет слушатели для переключения цвета волшебника

--- js/setup.js
+++ js/setup.js
@@ -3,8 +3,9 @@
 // константы для создания массива четырех волщебников-объектов
 var WIZARDS_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARDS_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var WIZARRDS_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARDS_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARDS_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARDS_FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
 
 // функция, генерирующая случайное число в диапазоне bottom - top
@@ -40,7 +41,7 @@ var wizards = [{}];
 for (var i = 0; i < 4; i++) {
   wizards.push({
     name: generateWizardName(),
-    coatColor: getRandomArrayItem(WIZARRDS_COAT_COLORS),
+    coatColor: getRandomArrayItem(WIZARDS_COAT_COLORS),
     eyesColor: getRandomArrayItem(WIZARDS_EYES_COLORS)
   });
 }
@@ -76,25 +77,30 @@ wizardList.appendChild(fragment);
 // document.querySelector('.setup').classList.remove('hidden');
 document.querySelector('.setup-similar').classList.remove('hidden');
 
+
 // ---------------------------------------------------------------------------
+// ОБРАБОТЧИКИ
 // ---------------------------------------------------------------------------
 
 var ENTER_KEYCODE = 13;
 var ESC_KEYCODE = 27;
-var setupOpen = document.querySelector('.setup-open');
 var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
 var setupClose = document.querySelector('.setup-close');
-var setupUserName = document.querySelector('.setup-user-name');
-var setupSubmit = document.querySelector('.setup-submit');
+var setupUserName = setup.querySelector('.setup-user-name');
+var setupWizardCoat = setup.querySelector('.setup-wizard .wizard-coat');
+var setupWizardEyes = setup.querySelector('.setup-wizard .wizard-eyes');
+var setupWizardFireball = document.querySelector('.setup-fireball-wrap');
 
 
+// закрывает окно и срабатывает на Esc
 var closePopup = function () {
   setup.classList.add('hidden');
   document.removeEventListener('keydown', onPopupEscDown);
 };
 
 
-// открывает попап, по нажатию Esc = закрывает, кроме случая, когда input в фокусе
+// открывает попап, по нажатию Esc - закрывает, кроме случая, когда input в фокусе
 var openPopup = function () {
   setup.classList.remove('hidden');
   document.addEventListener('keydown', onPopupEscDown);
@@ -106,29 +112,50 @@ var openPopup = function () {
   });
 };
 
+
+// закрывает окно по Esc
 var onPopupEscDown = function (evt) {
   if (evt.keyCode === ESC_KEYCODE) {
     closePopup();
   }
 };
 
+
+// открывает по клику
 setupOpen.addEventListener('click', openPopup);
 
+// открывает по нажатию на Enter
 setupOpen.addEventListener('keydown', function (evt) {
   if (evt.keyCode === ENTER_KEYCODE) {
     openPopup();
   }
 });
 
+
+// закрывает по клику
 setupClose.addEventListener('click', closePopup);
 
-setupSubmit.addEventListener('click', function (evt) {
-  closePopup();
-  evt.preventDefault();
-});
-
+// закрывает по нажатию на Enter
 setupClose.addEventListener('keydown', function (evt) {
   if (evt.keyCode === ENTER_KEYCODE) {
     closePopup();
   }
+});
+
+
+// меняет цвет мантии по клику
+setupWizardCoat.addEventListener('click', function () {
+  setupWizardCoat.style.fill = getRandomArrayItem(WIZARDS_COAT_COLORS);
+});
+
+
+// меняет цвет глаз по клику
+setupWizardEyes.addEventListener('click', function () {
+  setupWizardEyes.style.fill = getRandomArrayItem(WIZARDS_EYES_COLORS);
+});
+
+
+// меняет цвет файербола по клику
+setupWizardFireball.addEventListener('click', function () {
+  setupWizardFireball.style.background = getRandomArrayItem(WIZARDS_FIREBALL_COLORS);
 });
